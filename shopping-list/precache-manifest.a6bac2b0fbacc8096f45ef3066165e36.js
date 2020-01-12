152a47efa142a001bfbca6aa01266a441c38b994
Joshua Martine <josh.martine@mail.utoronto.ca>
2020-01-12 12:16:48 -0500

    Add shopping list demo

---
+++ shopping-list/precache-manifest.a6bac2b0fbacc8096f45ef3066165e36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc43101b23baf8fd8c3ab7d8e095d2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2565011976676e79f0ed",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "db754ec8a0f3719b8340",
+    "url": "/static/css/main.637483da.chunk.css"
+  },
+  {
+    "revision": "2565011976676e79f0ed",
+    "url": "/static/js/2.713d1e04.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.713d1e04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db754ec8a0f3719b8340",
+    "url": "/static/js/main.27f68618.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
